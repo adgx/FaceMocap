3da1178d86272b6909d57556ab928d23869f2d5c
F3de22 <141253124+F3de22@users.noreply.github.com>
2026-07-17 15:20:22 +0200

    Scheletro gui add-on + generazione armatura con ossa di base

---
+++ gui.py
@@ -0,0 +1,19 @@
+import bpy
+
+class FACEMOCAP_PT_main_panel(bpy.types.Panel):
+    """Crea un Pannello nella barra laterale (N) della Vista 3D"""
+    bl_label = "FaceMocap"
+    bl_idname = "FACEMOCAP_PT_main_panel"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'FaceMocap'
+
+    def draw(self, context):
+        layout = self.layout
+
+        layout.label(text="Sistema pronto", icon='OUTLINER_OB_ARMATURE')
+        
+        layout.separator()
+        
+        # bottone che chiama operators.py
+        layout.operator("facemocap.create_armature", text="Genera Armatura Facciale", icon='BONE_DATA')
